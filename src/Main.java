0649012271df2f69626c0b337dde97e33e2646c9
Star <creacciones@gmail.com>
2024-10-18 23:52:54 -0300

    Se crea un programa básico por consola, comando SALIR

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    //se utiliza la clase Scanner, usando su metodo escanner, dopnde se captura con System.in
+    Scanner scanner = new Scanner (System.in);
+    // se crea la variable del tipo String "entrada" y se le asigna valor ""
+    String entrada ="";
+    // Mientras que el usuario no escriba el comando Salir, le seguirá pidiendo escribir un comando
+    while (!entrada.equals("salir")) {
+        //loop de ingresa comando
+        System.out.println("Ingresa el comando: ");
+        //se captura lo ingresado por el usuario, se reemplaza el valor de "entrada" que era "" por lo capturado por el metodo scanner
+        //se ultiliza un metodo para cambiar toda letra a minúscula
+        entrada = scanner.next().toLowerCase();
 
-    Scanner scanner = new Scanner(System.in);
+    }
+    //si el usuario escribe "salir" se rompe el loop y escribre en pantalla
+        System.out.println("Felicidades, has logrado salir del Loopsad" );
+    }
+
+
+
+
+
+
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese un número: ");
         int numero = scanner.nextInt();
 
@@ -19,7 +40,7 @@ public class Main {
                 System.out.println("El número es divisible por 4");}
                      else if (numero % 3 == 0 ) {
                      System.out.println("El número es divisible por 3");}
-   }
+   }*/
 }
 
 
