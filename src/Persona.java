f44acd7f9a3e376930b875b9bb93341f9e5144a6
Star <creacciones@gmail.com>
2024-10-27 15:54:11 -0300

    Se agrega ejercicio de getter & setters

--- src/Persona.java
+++ src/Persona.java
@@ -9,4 +9,13 @@ public class Persona {
             this.nombre = nombre;
             this.edad = edad;
         }
+
+    public String getNombre() {
+        return nombre;}
+
+    public void setNombre(String nombre) {
+         this.nombre = nombre;
+
+
+    }
 }
